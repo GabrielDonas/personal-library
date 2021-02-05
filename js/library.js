c0ed8f2275de96a9cacdaa1ebcb9f056e44c320d
Gabriel Donas <osradecky@gmail.com>
2021-02-05 11:41:28 +0000

    Add all features to library.js

--- js/library.js
+++ js/library.js
@@ -12,11 +12,12 @@ function Book(title, author, pages) {
   this.title = title;
   this.author = author;
   this.pages = pages;
+  this.read = false;
   this.id = null;
 }
 
-Book.prototype.readStatus = function () {
-  alert("Hi");
+Book.prototype.readingStatus = function () {
+  this.read = this.read ? false : true;
 };
 
 function loadLibrary() {
@@ -25,25 +26,45 @@ function loadLibrary() {
   myLibrary.forEach((element, index) => {
     element.id = index;
     let bookInfo = document.createElement("div");
-    bookInfo.className = "book";
+    bookInfo.className = element.read ? "book-finished" : "book";
     bookContainer.appendChild(bookInfo);
 
     const appendSection = (section, sectionName) => {
-      section.innerHTML =
-        sectionName == "remove" ? sectionName : element[sectionName];
+      switch (sectionName) {
+        case "remove":
+          section.innerHTML = sectionName;
+          break;
+        case "author":
+          section.innerHTML = "by " + element[sectionName];
+          break;
+        case "pages":
+          section.innerHTML = element[sectionName] + " pages";
+          break;
+        default:
+          section.innerHTML = element[sectionName];
+      }
       section.className = sectionName;
       bookInfo.appendChild(section);
     };
 
     let title = document.createElement("div");
     appendSection(title, "title");
+
     let author = document.createElement("div");
     appendSection(author, "author");
+
     let pages = document.createElement("div");
     appendSection(pages, "pages");
+
     let readCheckBox = document.createElement("input");
     appendSection(readCheckBox, "readCheckBox");
     readCheckBox.type = "checkbox";
+    readCheckBox.checked = element.read ? true : false;
+    readCheckBox.addEventListener("click", () => {
+      element.readingStatus();
+      loadLibrary();
+    });
+
     let removeButton = document.createElement("button");
     appendSection(removeButton, "remove");
     removeButton.addEventListener("click", () => {
@@ -62,5 +83,3 @@ function addBookToLibrary() {
 addBookButton.onclick = () => {
   addBookToLibrary();
 };
-
-// 3 - read status associate with a prototype
